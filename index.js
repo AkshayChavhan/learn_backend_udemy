179b2d211bb7da347ca65b8a220d8eea697d4d98
Akshay Chavhan <86603141+AkshayChavhan@users.noreply.github.com>
2022-09-18 20:53:25 +0530

    231
    Creating server,sending response ,creating routes
    Installing nodemon

--- index.js
+++ index.js
@@ -2,8 +2,31 @@ console.log('Node running ');
 
 const express = require('express');
 
-const  app = express() ;
+const  app = express();
 
-app.listen(3000 ,() => {
-    console.log('server started on port number 3000');
+// what should happen whrn server request GET from browser
+// callback function will tell what to do 
+
+app.get('/',(request,response)=>{
+// console.log(request);     //this will return you the object in request
+// response.send('Hello'); //this will send Hello in browser
+response.send('<h1>Hello World </h1>'); //this will send Hello Word as H1 tag in browser
 })
+
+
+app.get('/contact' ,(req,res) =>{
+    res.send('I am at contact pages');
+})
+
+app.get('/about' ,(req,res) =>{
+    res.send('I am at about pages');
+})
+
+app.get('/hobbies' ,(req,res) =>{
+res.send('<ul><li>Hobbies 1</li><li>Hobbies 2</li><li>Hobbies 3</li></ul>');
+})
+
+
+app.listen(3005 ,() => {
+    console.log('server started on port number 3005');
+});
