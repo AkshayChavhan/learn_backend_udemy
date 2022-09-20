c2993a89ab9cfb6dedc5e6156332dcc07ee8dc61
Akshay Chavhan <86603141+AkshayChavhan@users.noreply.github.com>
2022-09-20 16:38:35 +0530

    Post request working
    After giving several inputs and click on calculate ,you will get post request successfully

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-console.log('Node running ');
-
 const express = require('express');
+// install body parser as "npm install body-parser" from terminal
+const bodyParser = require('body-parser');      //importing bodyParser 
 
-const  app = express() ;
 
-app.listen(3000 ,() => {
-    console.log('server started on port number 3000');
-})
+const  app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/' ,function(req,res){
+    res.sendFile(__dirname +'/index.html');
+});
+
+// this is the post response for route '/' 
+app.post('/',function(req,res){
+    var number1 = Number(req.body.num1);
+    var number2 = Number(req.body.num2);
+
+    var result = number1 + number2 ;
+
+    res.send("The result of the calculator is " + result);
+});
+
+
+app.listen(3005 ,() => {
+    console.log('server started on port number 3005');
+});
