e6e6f23566967e51f68806768efd2ff0baaf6e8c
Akshay Chavhan <86603141+AkshayChavhan@users.noreply.github.com>
2022-09-21 18:20:23 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-console.log('Node running ');
-
-const express = require('express');
-
-const  app = express() ;
-
-app.listen(3000 ,() => {
-    console.log('server started on port number 3000');
-})
